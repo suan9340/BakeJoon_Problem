249671457e65e7c2440dd90fc4d8f2209aa086a3
suan9340 <sue9340@gmail.com>
2022-05-18 16:19:05 +0900

    테스트 올리기

--- 백쥰쥰/1929.cpp
+++ 백쥰쥰/1929.cpp
@@ -1,18 +1,34 @@
-#include <iostream>
-
-#define MAXNUM 1000000
-using namespace std;
-
-int arr[MAXNUM + 1];
-int m, n;
-int main()
-{
-	cin >> m >> n;
-
-	for (int i = 2; i <= n; i++)
-	{
-		arr[i] = i;
-	}
-
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//#define MAX 1000000
+//
+//int arr[MAX + 1]{ 0, };
+//
+//int main()
+//{
+//	int m, n;
+//	cin >> m >> n;
+//
+//	// 초기 배열 설정
+//	for (int i = 2; i <= n; i++)
+//		arr[i] = i;
+//
+//	// 2 부터 지정 수 까지 삭제
+//	for (int i = 2; i * i <= n; i++)
+//	{
+//		if (arr[i] == 0)
+//			continue;
+//
+//		for (int j = i * i; j <= n; j += i)
+//			arr[j] = 0;
+//	}
+//
+//	// 배열에서 값이 0인거 뺴고다 출력
+//	for (int i = m; i <= n; i++)
+//	{
+//		if (arr[i] != 0)
+//			cout << arr[i] << '\n';
+//	}
+//	return 0;
+//}
