5630f3338f18338c3e366fb78e8032fa2ba69f2e
suan9340 <sue9340@gmail.com>
2022-05-29 22:04:09 +0900

    1문제 푼거!!

---
+++ 백쥰쥰/10250.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//#include <cmath>
+//using namespace std;
+//
+//int h, w, n, flor, num, t;
+//int main()
+//{
+//	cin >> t;
+//
+//	for (int i = 0; i < t; i++)
+//	{
+//		cin >> h >> w >> n;
+//
+//		flor = n % h;
+//		if (flor == 0) flor = h;
+//		num = ceil((float)n / h);
+//		cout << flor * 100 + num << endl;
+//	}
+//
+//	return 0;
+//}
