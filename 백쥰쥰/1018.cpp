ff03cbdd7b993465a7dc19dc3f41a641b9708977
suan9340 <sue9340@gmail.com>
2022-05-31 13:55:19 +0900

    체스판 드디어 풀었ㄸ,,

--- 백쥰쥰/1018.cpp
+++ 백쥰쥰/1018.cpp
@@ -2,35 +2,83 @@
 //#include <stack>
 //using namespace std;
 //
-//char arr[51][51];
-//int n, m;
-//string input;
+//int n, m, cnt, result = 64;
+//string board[51];
+//string WB[8] =
+//{
+//	"WBWBWBWB",
+//	"BWBWBWBW",
+//	"WBWBWBWB",
+//	"BWBWBWBW",
+//	"WBWBWBWB",
+//	"BWBWBWBW",
+//	"WBWBWBWB",
+//	"BWBWBWBW",
+//};
+//
+//string BB[8] =
+//{
+//	"BWBWBWBW",
+//	"WBWBWBWB",
+//	"BWBWBWBW",
+//	"WBWBWBWB",
+//	"BWBWBWBW",
+//	"WBWBWBWB",
+//	"BWBWBWBW",
+//	"WBWBWBWB",
+//};
+//
 //char a;
+//
+//int WhiteFirst(int a, int b);
+//int BlackFirst(int a, int b);
 //
 //int main()
 //{
 //	cin >> n >> m;
 //
 //	for (int i = 0; i < n; i++)
+//		cin >> board[i];
+//
+//	for (int i = 0; i < n - 7; i++)
 //	{
-//		cin >> input;
-//		for (int j = 0; j < m; j++)
+//		for (int j = 0; j < m - 7; j++)
 //		{
-//			a = input[j];
-//			
-//			// 처음 시작이 W인 경우
-//			if (a == 'W')
-//			{
-//
-//			}
-//
-//			// 처음 시작이 B인 경우
-//			else if (a == 'B')
-//			{
-//
-//			}
+//			WhiteFirst(j, i);
+//			BlackFirst(j, i);
 //		}
-//		cout << "\n";
 //	}
+//
+//	cout << result;
 //	return 0;
+//}
+//
+//int WhiteFirst(int a, int b)
+//{
+//	cnt = 0;
+//	for (int i = 0; i < 8; i++)
+//	{
+//		for (int j = 0; j < 8; j++)
+//		{
+//			if (WB[i][j] != board[b + i][a + j])
+//				cnt++;
+//		}
+//	}
+//	if (cnt < result) result = cnt;
+//	return cnt;
+//}
+//
+//int BlackFirst(int a, int b)
+//{
+//	cnt = 0;
+//	for (int i = 0; i < 8; i++)
+//	{
+//		for (int j = 0; j < 8; j++)
+//		{
+//			if (BB[i][j] != board[b + i][a + j])
+//				cnt++;
+//		}
+//	}
+//	if (cnt < result) result = cnt;
+//	return cnt;
 //}
