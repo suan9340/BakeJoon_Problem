33545c82f270da9403be89b5a33415c6f3866492
suan9340 <sue9340@gmail.com>
2022-06-01 17:46:27 +0900

    풀어보자 그리디.!

---
+++ 백쥰쥰/2839.cpp
@@ -0,0 +1,23 @@
+//#include <iostream>
+//using namespace std;
+//
+//int n, total;
+//int main()
+//{
+//	cin >> n;
+//
+//	while (n >= 0)
+//	{
+//		if (n % 5 == 0)
+//		{
+//			total += (n / 5);
+//			cout << total << endl;
+//			return 0;
+//		}
+//		n -= 3;
+//		total++;
+//	}
+//
+//	cout << "-1";
+//	return 0;
+//}
