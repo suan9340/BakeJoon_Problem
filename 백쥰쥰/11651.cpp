58765c273ea310e39e2f028ec9eabb6f17d515f5
suan9340 <sue9340@gmail.com>
2022-06-02 20:28:00 +0900

    달팽이!

---
+++ 백쥰쥰/11651.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+vector<pair<int, int>> v;
+int cnt, a, b;
+
+bool Compare(pair<int, int> a, pair<int, int> b);
+
+int main()
+{
+	cin >> cnt;
+
+	for (int i = 0; i < cnt; i++)
+	{
+		cin >> a >> b;
+		v.push_back({ a,b });
+	}
+
+	sort(v.begin(), v.end(), Compare);
+
+	for (int i = 0; i < cnt; i++)
+		cout << v[i].first << " " << v[i].second << "\n";
+
+	return 0;
+}
+
+bool Compare(pair<int, int> a, pair<int, int> b)
+{
+	if (a.second == b.second)
+		return a.first < b.first;
+	else
+		return a.second < b.second;
+}
