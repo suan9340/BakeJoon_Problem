6691c63bf8ed31df29d5909b39f96eebfa6bc9a8
suan <sue9340@gmail.com>
2022-11-23 00:13:58 +0900

    11.23

---
+++ 백쥰쥰/1436.cpp
@@ -0,0 +1,33 @@
+//#include <iostream>	
+//using namespace std;
+//
+//int n, dead, cnt, ans;
+//int main()
+//{
+//	cin >> n;
+//
+//
+//	while (cnt != n)
+//	{
+//		ans++;
+//		dead = ans;
+//
+//		while (dead != 0)
+//		{
+//			if (dead % 1000 == 666)
+//			{
+//				cnt++;
+//				break;
+//			}
+//			else
+//			{
+//				dead /= 10;
+//			}
+//		}
+//
+//	}
+//
+//	cout << ans;
+//
+//	return 0;
+//}
--- 백쥰쥰/1463.cpp
+++ 백쥰쥰/1463.cpp
@@ -1,13 +1,13 @@
-#include <iostream>
-using namespace std;
-
-long long input;
-int cnt;
-
-int main()
-{
-	cin >> input;
-
-
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//long long input;
+//int cnt;
+//
+//int main()
+//{
+//	cin >> input;
+//
+//
+//	return 0;
+//}
