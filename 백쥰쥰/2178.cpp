d0c305fcf55810842b84ae962adb01bb61442f51
suan <sue9340@gmail.com>
2022-11-28 00:29:17 +0900

    11.28

--- 백쥰쥰/2178.cpp
+++ 백쥰쥰/2178.cpp
@@ -1,63 +1,59 @@
-//#include <iostream>
-//#include <queue>	
-//using namespace std;
-//
-//int BFS(int v);
-//
-//#define MAX 100
-//
-//int n, m, maze[MAX][MAX], visited[MAX][MAX];
-//string temp;
-//
-//int dy[4] = { -1,0,1,0 };
-//int dx[4] = { 0,1,0,-1 };
-//
-//queue < pair<int, int>> q;
-//
-//int main()
-//{
-//	cin >> n >> m;
-//
-//	for (int i = 0; i < n; i++)
-//	{
-//		cin >> temp;
-//
-//		for (int j = 0; j < m; j++)
-//		{
-//			maze[i][j] = temp[j] - '0';
-//		}
-//	}
-//
-//	BFS(0);
-//
-//	return 0;
-//}
-//
-//int BFS(int v)
-//{
-//	visited[0][0] = 1;
-//	q.push({ 0,0 });
-//
-//	while (q.size())
-//	{
-//		int _y = q.front().first;
-//		int _x = q.front().second;
-//
-//		q.pop();
-//
-//		for (int i = 0; i < 4; i++)
-//		{
-//			int _ny = _y + dy[i];
-//			int _nx = _x + dx[i];
-//
-//			if (_ny >=0 && _ny < n && _nx >=0 && _nx < m && visited[_ny][_nx] != 0 && maze[_ny][_nx] !=0) continue;
-//
-//			visited[_ny][_nx] = visited[_ny][_nx] + 1;
-//			q.push({ _ny,_nx });
-//		}
-//
-//	}
-//
-//	cout << visited[n - 1][m - 1];
-//	return 0;
-//}
+#include <iostream>
+#include <queue>	
+using namespace std;
+
+void BFS(int x, int y);
+
+int n, m;
+int check[101][101];
+bool visit[101][101];
+char map[101][101];
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,1,0,-1 };
+
+queue<pair<int, int>> q;
+
+int main()
+{
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> map[i];
+	}
+
+	BFS(0, 0);
+	cout << check[n - 1][m - 1] + 1;
+
+	return 0;
+}
+
+void BFS(int x, int y)
+{
+	visit[x][y] = true;
+	q.push({ x,y });
+
+	while (!q.empty())
+	{
+		int x = q.front().first;
+		int y = q.front().second;
+
+		q.pop();
+
+		for (int i = 0; i < 4; i++)
+		{
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+
+			if (nx >= 0 && nx < n && ny >= 0 && ny < m)
+			{
+				if (map[nx][ny] == '1' && visit[nx][ny] == false)
+				{
+					check[nx][ny] = check[x][y] + 1;
+					visit[nx][ny] = true;
+					q.push({ nx,ny });
+				}
+			}
+		}
+	}
+}	
--- 백쥰쥰/25304.cpp
+++ 백쥰쥰/25304.cpp
@@ -1,24 +1,24 @@
-#include <iostream>
-using namespace std;
-
-int x, n, a, b, total;
-
-int main()
-{
-	cin >> x;
-	cin >> n;
-
-	for (int i = 0; i < n; i++)
-	{
-		cin >> a >> b;
-
-		total += (a * b);
-	}
-
-	if (total == x)
-		cout << "Yes";
-	else
-		cout << "No";	
-
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//int x, n, a, b, total;
+//
+//int main()
+//{
+//	cin >> x;
+//	cin >> n;
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		cin >> a >> b;
+//
+//		total += (a * b);
+//	}
+//
+//	if (total == x)
+//		cout << "Yes";
+//	else
+//		cout << "No";	
+//
+//	return 0;
+//}
