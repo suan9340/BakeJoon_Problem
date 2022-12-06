f0d79524a8b0a8a50c2e456e0edd9ce633b75a88
suan <sue9340@gmail.com>
2022-12-06 16:38:53 +0900

    12.06

---
+++ 백쥰쥰/10870.cpp
@@ -0,0 +1,27 @@
+//#include <iostream>
+//using namespace std;
+//int Fibo(int _n);
+//
+//int num;
+//int main()
+//{
+//	cin >> num;
+//	cout << Fibo(num);
+//	return 0;
+//}
+//
+//int Fibo(int _n)
+//{
+//	if (_n >= 2)
+//	{
+//		return Fibo(_n - 1) + Fibo(_n - 2);
+//	}
+//	else if (_n == 1)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return 0;
+//	}
+//}
--- 백쥰쥰/4949.cpp
+++ 백쥰쥰/4949.cpp
@@ -1,60 +1,60 @@
-#include <iostream>
-#include <string>
-#include <stack>
-using namespace std;
-
-string s;
-
-int main()
-{
-	while (true)
-	{
-		getline(cin, s);
-		stack<char> c;
-
-		if (s[0] == '.') break;
-
-		for (int i = 0; i < s.length(); i++)
-		{
-			if (s[i] == '(') c.push('(');
-			if (s[i] == '[') c.push('[');
-
-			if (s[i] == ')')
-			{
-				if (!c.empty() && c.top() == '(')
-				{
-					c.pop();
-				}
-				else
-				{
-					cout << "no\n";
-					break;
-				}
-			}
-
-
-			if (s[i] == ']')
-			{
-				if (!c.empty() && c.top() == '[')
-				{
-					c.pop();
-				}
-				else
-				{
-					cout << "no\n";
-					break;
-				}
-			}
-
-			if (c.empty() && i == s.length() - 2)
-			{
-				cout << "yes\n";
-			}
-			else if (!c.empty() && i == s.length() - 2)
-			{
-				cout << "no\n";
-			}
-		}
-	}
-	return 0;
-}
+//#include <iostream>
+//#include <string>
+//#include <stack>
+//using namespace std;
+//
+//string s;
+//
+//int main()
+//{
+//	while (true)
+//	{
+//		getline(cin, s);
+//		stack<char> c;
+//
+//		if (s[0] == '.') break;
+//
+//		for (int i = 0; i < s.length(); i++)
+//		{
+//			if (s[i] == '(') c.push('(');
+//			if (s[i] == '[') c.push('[');
+//
+//			if (s[i] == ')')
+//			{
+//				if (!c.empty() && c.top() == '(')
+//				{
+//					c.pop();
+//				}
+//				else
+//				{
+//					cout << "no\n";
+//					break;
+//				}
+//			}
+//
+//
+//			if (s[i] == ']')
+//			{
+//				if (!c.empty() && c.top() == '[')
+//				{
+//					c.pop();
+//				}
+//				else
+//				{
+//					cout << "no\n";
+//					break;
+//				}
+//			}
+//
+//			if (c.empty() && i == s.length() - 2)
+//			{
+//				cout << "yes\n";
+//			}
+//			else if (!c.empty() && i == s.length() - 2)
+//			{
+//				cout << "no\n";
+//			}
+//		}
+//	}
+//	return 0;
+//}
