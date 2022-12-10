00b6ba5db2593216311488a6ea9fbfe82fb67d7b
suan <sue9340@gmail.com>
2022-12-10 21:03:52 +0900

    12.10

---
+++ 백쥰쥰/1427.cpp
@@ -0,0 +1,25 @@
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//using namespace std;
+//
+//string input;
+//vector<char> v;
+//int main()
+//{
+//	cin >> input;
+//
+//	for (int i = 0; i < input.size(); i++)
+//	{
+//		v.push_back(input[i]);
+//	}
+//
+//	sort(v.begin(), v.end());
+//
+//	for (int i = v.size() - 1; i >= 0; i--)
+//	{
+//		cout << v[i];
+//	}
+//
+//	return 0;
+//}
---
+++ 백쥰쥰/25305.cpp
@@ -0,0 +1,24 @@
+//#include <iostream>	
+//#include <vector>
+//#include <algorithm>
+//using namespace std;
+//
+//int n, k, input;
+//vector<int> v;
+//
+//int main()
+//{
+//	cin >> n >> k;
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		cin >> input;
+//		v.push_back(input);
+//	}
+//
+//	sort(v.begin(), v.end(), greater<>());
+//
+//	cout << v[k -1];
+//
+//	return 0;
+//}
