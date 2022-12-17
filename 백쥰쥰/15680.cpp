07156cd7fe08a0efec3b40cb0087932d9f8502c5
suan <sue9340@gmail.com>
2022-12-17 23:08:38 +0900

    12.17

---
+++ 백쥰쥰/15680.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int input;
+//int main()
+//{
+//	cin >> input;
+//
+//	if (input == 0)
+//	{
+//		cout << "YONSEI";
+//	}
+//	else
+//	{
+//		cout << "Leading the Way to the Future";
+//	}
+//	return 0;
+//}
