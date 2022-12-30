b92ca6b6ec82939a7a07abdf93c0c0e5129b284a
suan <sue9340@gmail.com>
2022-12-30 21:10:50 +0900

    12.30

---
+++ 백쥰쥰/1316.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int n;
+set<char> s;
+string input;
+
+int main()
+{
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> input;
+
+		for (int j = 0; j < input.length(); j++)
+		{
+			char _c = input[j];
+			s.insert(_c);
+		}
+	}
+
+	return 0;
+}
