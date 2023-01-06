6a6e31aac222189f94136cdb848b175c35132d2c
suan <sue9340@gmail.com>
2023-01-06 22:29:40 +0900

    01.06

---
+++ 백쥰쥰/1541.cpp
@@ -0,0 +1,43 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//string input;
+//string num;
+//int result = 0;
+//bool isMinus = false;
+//
+//int main()
+//{
+//	cin >> input;
+//
+//
+//	for (int i = 0; i <= input.size(); i++)
+//	{
+//
+//		if (input[i] == '-' || input[i] == '+' || i == input.size())
+//		{
+//			if (isMinus)
+//			{
+//				result -= stoi(num);
+//				num = "";
+//			}
+//			else
+//			{
+//				result += stoi(num);
+//				num = "";
+//			}
+//		}
+//		else
+//		{
+//			num += input[i];
+//		}
+//
+//		if (input[i] == '-')
+//		{
+//			isMinus = true;
+//		}
+//	}
+//
+//	cout << result;
+//}
---
+++ 백쥰쥰/2440.cpp
@@ -0,0 +1,18 @@
+//#include <iostream>	
+//using namespace std;
+//
+//int n;
+//int main()
+//{
+//	cin >> n;
+//
+//	for (int i = n; i >= 0; i--)
+//	{
+//		for (int j = 0; j < i; j++)
+//		{
+//			cout << "*";
+//		}
+//		cout << "\n";
+//	}
+//	return 0;
+//}
