8dd56279042295a59cbde512e7c41fe4cd8e3ed0
suan._.0618 <77601060+suan9340@users.noreply.github.com>
2023-03-27 11:38:02 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int DP[1000001] = { 0 };
+
+// 10 -> 9 -> 3 -> 1 = 3번
+int main()
+{
+	int N;
+	cin >> N;
+
+
+	for (int i = 2; i <= N; i++)
+	{
+		DP[i] = 1 + DP[i - 1];
+
+		if (i % 3 == 0)
+		{
+			DP[i] = min(DP[i], 1 + DP[i / 3]);
+		}
+		if (i % 2 == 0)
+		{
+			DP[i] = min(DP[i], 1 + DP[i / 2]);
+		}
+	}
+
+	cout << DP[N];
+
+	return 0;
+}
