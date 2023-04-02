08a3e26c5be9791c72de7eaafe813820b4c3dc10
suan <sue9340@gmail.com>
2023-04-03 00:15:53 +0900

    4.3

---
+++ 백쥰쥰/25206.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string name, ave;
+double score, dave, total, ssco;
+
+double returnScore(string _input)
+{
+	if (_input == "A+")
+	{
+		dave = 4.5;
+	}
+	else if (_input == "A0")
+	{
+		dave = 4.0;
+	}
+	else if (_input == "B+")
+	{
+		dave = 3.5;
+	}
+	else if (_input == "B0")
+	{
+		dave = 3.0;
+	}
+	else if (_input == "C+")
+	{
+		dave = 2.5;
+	}
+	else if (_input == "C0")
+	{
+		dave = 2.0;
+	}
+	else if (_input == "D+")
+	{
+		dave = 1.5;
+	}
+	else if (_input == "D0")
+	{
+		dave = 1.0;
+	}
+	else if (_input == "F")
+	{
+		dave = 0.0;
+	}
+
+	return dave;
+}
+
+
+int main()
+{
+	for (int i = 0; i < 20; i++)
+	{
+		cin >> name >> score >> ave;
+
+		if (ave != "P")
+		{
+			ssco += score;
+			total += score * returnScore(ave);
+		}
+	}
+
+	total /= ssco;
+
+	cout << fixed;  
+	cout.precision(6);  
+	cout << total;
+
+	return 0;
+}
+
---
+++ 백쥰쥰/2941.cpp
@@ -0,0 +1,16 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//string input;
+//int main()
+//{
+//	cin >> input;
+//
+//	if (!input.find("e"))
+//	{
+//		cout << "find";
+//	}
+//
+//	return 0;
+//}
