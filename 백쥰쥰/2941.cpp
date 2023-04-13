cebc870ad4e92c84dbd14fc906ec3f6eca44bd71
suan <sue9340@gmail.com>
2023-04-14 00:45:31 +0900

    4.14

--- 백쥰쥰/2941.cpp
+++ 백쥰쥰/2941.cpp
@@ -1,16 +1,45 @@
 //#include <iostream>
 //#include <string>
+//#include <vector>
 //using namespace std;
+//void SettingAlpha();
 //
 //string input;
+//vector<string> valpha;
+//int idx;
+//
 //int main()
 //{
 //	cin >> input;
+//	SettingAlpha();
 //
-//	if (!input.find("e"))
+//	for (int i = 0; i < valpha.size(); i++)
 //	{
-//		cout << "find";
+//		while (true)
+//		{
+//			idx = input.find(valpha[i]);
+//			if (idx == string::npos)
+//			{
+//				break;
+//			}
+//
+//			input.replace(idx, valpha[i].length(), "$");
+//		}
 //	}
 //
+//	cout << input.length();
+//
 //	return 0;
+//}
+//
+//void SettingAlpha()
+//{
+//	valpha.push_back("c=");
+//	valpha.push_back("c-");
+//	valpha.push_back("dz=");
+//	valpha.push_back("d-");
+//	valpha.push_back("lj");
+//	valpha.push_back("nj");
+//	valpha.push_back("s=");
+//	valpha.push_back("z=");
 //}
