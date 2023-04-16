1dc6d39a5cf62ae30a56e47112cb84478e945fc6
suan <sue9340@gmail.com>
2023-04-16 21:40:57 +0900

    4.16

---
+++ 백쥰쥰/10798.cpp
@@ -0,0 +1,31 @@
+//#include <iostream>	
+//using namespace std;
+//
+//char arr[5][15];
+//string input;
+//
+//int main()
+//{
+//	for (int i = 0; i < 5; i++)
+//	{
+//		cin >> input;
+//
+//		for (int j = 0; j < input.size(); j++)
+//		{
+//			arr[i][j] = input[j];
+//		}
+//	}
+//
+//	for (int i = 0; i < 15; i++)
+//	{
+//		for (int j = 0; j < 5; j++)
+//		{
+//			if (arr[j][i] == NULL)
+//			{
+//				continue;
+//			}
+//			cout << arr[j][i];
+//		}
+//	}
+//	return 0;
+//}
