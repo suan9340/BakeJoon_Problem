05892efbe073816a617f805ed660144d228a32ce
suan <sue9340@gmail.com>
2023-05-10 23:25:37 +0900

    5.10

---
+++ 백쥰쥰/7785.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+unordered_map<string, int> m;
+string name, ace;
+int num;
+
+int main()
+{
+	cin >> num;
+
+	for (int i = 0; i < num; i++)
+	{
+		cin >> name >> ace;
+
+		if()
+	}
+
+	return 0;
+}
